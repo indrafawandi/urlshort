fd1ed928b2aea326bc6315824d9ff58a60f1c25d
Fawandi <indraf.infra@dwara.co.id>
2026-09-22 12:46:58 +0700

    fix(bootstrap): match GitHub's immutable-ID OIDC subject claim format

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -101,7 +101,7 @@ data "aws_iam_policy_document" "github_oidc_assume" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org}/${var.github_repo}:*"]
+      values = ["repo:${var.github_org}@*/${var.github_repo}@*:*"]
     }
   }
 }
